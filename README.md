521dbdd24d4d15d7698c610939dae3602da8ee7c
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2020-01-16 11:26:35 +0900

    Add c/step3.c c/step4.c

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # My SDL2 warehouse
+
+## Requires
+
+sdl2, sdl2_image
+
+## c
+
+```
+$ make SRCS=ファイル.c
+$ ./app
+```
---
+++ c/step3.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_image.h>
+
+#define TITLE "My SDL2 Window"
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(TITLE,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          WINDOW_WIDTH,
+                                          WINDOW_HEIGHT,
+                                          0);
+
+    if (!window) {
+        printf("Error creating window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    
+    SDL_Renderer *rend = SDL_CreateRenderer(window,
+                                            -1,
+                                            render_flags);
+
+    if (!rend) {
+        printf("Error creating renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Surface *surface = IMG_Load("imgs/newyearalien2.png");
+
+    if (!surface) {
+        printf("Error creating surface\n");
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+
+    if (!tex) {
+        printf("Error creating texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_RenderClear(rend);
+    SDL_RenderCopy(rend, tex, NULL, NULL);
+    SDL_RenderPresent(rend);
+    
+    SDL_Delay(5000);
+
+    // Clean up
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
