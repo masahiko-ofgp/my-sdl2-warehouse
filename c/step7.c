de8853ffa001d1881fd6989046f3b23cf7ce7f02
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2020-01-17 17:02:28 +0900

    Add c/step7.c

---
+++ c/step7.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_image.h>
+
+#define TITLE "MY SDL2 Window"
+#define WINDOW_WIDTH (600)
+#define WINDOW_HEIGHT (600)
+
+#define SPEED (300)
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+            TITLE,
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
+            0
+    );
+
+    if (!window) {
+        printf("Could not create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+    SDL_Renderer *rend =SDL_CreateRenderer(window, -1, render_flags);
+    
+    if (!rend) {
+        printf("error creating renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Surface *surface = IMG_Load("../imgs/newyearalien2.png");
+    if (!surface) {
+        printf("error creating surface\n");
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+    if (!tex) {
+        printf("error creating texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Rect dest;
+
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+    dest.w /= 4;
+    dest.h /= 4;
+
+    float x_pos = (WINDOW_WIDTH - dest.w) / 2;
+    float y_pos = (WINDOW_HEIGHT - dest.h) / 2;
+    float x_vel = 0;
+    float y_vel = 0;
+
+    int close_requested = 0;
+
+    while (!close_requested) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                close_requested = 1;
+            }
+        }
+
+        int mouse_x, mouse_y;
+        int buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
+
+        int target_x = mouse_x - dest.w / 2;
+        int target_y = mouse_y - dest.h / 2;
+        float delta_x = target_x - x_pos;
+        float delta_y = target_y - y_pos;
+        float distance = sqrt(delta_x * delta_x + delta_y * delta_y);
+
+        if (distance < 5) {
+            x_vel = y_vel = 0;
+        } else {
+            x_vel = delta_x * SPEED / distance;
+            y_vel = delta_y * SPEED / distance;
+        }
+
+        if (buttons & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+            x_vel = -x_vel;
+            y_vel = -y_vel;
+        }
+
+        x_pos += x_vel / 60;
+        y_pos += y_vel / 60;
+
+        if (x_pos <= 0) x_pos = 0;
+        if (y_pos <= 0) y_pos = 0;
+        if (x_pos >= WINDOW_WIDTH - dest.w) x_pos = WINDOW_WIDTH - dest.w;
+        if (y_pos >= WINDOW_HEIGHT - dest.h) y_pos = WINDOW_HEIGHT - dest.h;
+
+        dest.y = (int) y_pos;
+        dest.x = (int) x_pos;
+
+        SDL_RenderClear(rend);
+
+        SDL_RenderCopy(rend, tex, NULL, &dest);
+        SDL_RenderPresent(rend);
+
+        SDL_Delay(1000/60);
+    }
+
+    // Clean up
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
